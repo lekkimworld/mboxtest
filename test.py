d2749fab7d16cd78eb073556e6a9b94e68fa2902
Mikkel Flindt Heisterberg <mheisterberg@salesforce.com>
2017-06-21 08:22:03 +0200

    Initial commit

---
+++ test.py
@@ -0,0 +1,24 @@
+import mailbox
+
+mbox = mailbox.mbox('test.mbox')
+for msg in mbox:
+	msgsubject = msg['subject']
+	msgfrom = msg['from']
+	msgbody = '';
+
+	# see if message is MIME multipart - if yes only get plain text parts
+	if msg.is_multipart():
+		for payload in msg.get_payload():
+			if payload['Content-Type'] == 'text/plain':
+				msgbody += payload.get_payload()
+	else:
+		# it's not -- great - just get contents
+		msgbody = msg.get_payload()
+
+	# ignore if no plain text body
+	if len(msgbody) == 0: continue
+
+	# output body length
+	print len(msgbody)
+
+    
